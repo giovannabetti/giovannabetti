5a3f5beadb164be4040fd19a8b7bdf1fa457a943
Giovanna Betti <giovannabetti@gmail.com>
2020-10-13 20:09:50 -0300

    Finalizando backend

--- code/giovannabetti/happy-app/backend/src/database/migrations/1602601123858-create_orphanages.ts
+++ code/giovannabetti/happy-app/backend/src/database/migrations/1602601123858-create_orphanages.ts
@@ -39,6 +39,10 @@ export class createOrphanages1602601123858 implements MigrationInterface {
           type: 'text',
         },
         {
+          name: 'opening_hours',
+          type: 'varchar'
+        },
+        {
           name: 'open_on_weekends',
           type: 'boolean',
           default: false,
--- code/giovannabetti/happy-app/backend/src/server.ts
+++ code/giovannabetti/happy-app/backend/src/server.ts
@@ -1,14 +1,22 @@
-import express, { response } from 'express';
+import express from 'express';
+import path from 'path';
+import cors from 'cors';
+
+import 'express-async-errors';
 
 import './database/connection';
 
+import routes from './routes';
+import errorHandler from './errors/handler';
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandler);
 
-app.get('/users', (req, res) => {
-    return response.json({ message: 'Hello World' }); 
-});
 
 app.listen(3333);
 
