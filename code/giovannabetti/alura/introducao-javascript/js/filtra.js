0df8801836fe6cadbe3d35141cf796551855878a
Giovanna Betti <giovannabetti@gmail.com>
2020-11-04 22:36:05 -0300

    Trabalhando com AJAX e finalizando projeto

--- code/giovannabetti/alura/introducao-javascript/js/filtra.js
+++ code/giovannabetti/alura/introducao-javascript/js/filtra.js
@@ -1,14 +1,14 @@
-var campoFiltro = document.querySelector("#filtrar-tabela");
+let campoFiltro = document.querySelector("#filtrar-tabela");
 
 campoFiltro.addEventListener("input", function() {
-    var pacientes = document.querySelectorAll(".paciente");
+    let pacientes = document.querySelectorAll(".paciente");
 
     if (this.value.length > 0) {
-        for (var i = 0; i < pacientes.length; i++) {
-            var paciente = pacientes[i];
-            var tdNome = paciente.querySelector(".info-nome");
-            var nome = tdNome.textContent;
-            var expressao = new RegExp(this.value, "i");
+        for (let i = 0; i < pacientes.length; i++) {
+            let paciente = pacientes[i];
+            let tdNome = paciente.querySelector(".info-nome");
+            let nome = tdNome.textContent;
+            let expressao = new RegExp(this.value, "i");
 
             // Adição aqui
             if (expressao.test(nome)) {
@@ -18,8 +18,8 @@ campoFiltro.addEventListener("input", function() {
             }
         }
     } else {
-        for (var i = 0; i < pacientes.length; i++) {
-            var paciente = pacientes[i];
+        for (let i = 0; i < pacientes.length; i++) {
+            let paciente = pacientes[i];
             paciente.classList.remove("invisivel");
         }
     }
--- code/giovannabetti/alura/introducao-javascript/js/form.js
+++ code/giovannabetti/alura/introducao-javascript/js/form.js
@@ -3,27 +3,29 @@ botaoAdicionar.addEventListener("click", function(event) {
 
     let form = document.querySelector("#form-adiciona");
     let paciente = obtemPacienteDoFormulario(form);
-
     let erros = validaPaciente(paciente);
-
     if (erros.length > 0) {
         exibeMensagensDeErro(erros);
         return;
     }
-    var pacienteTr = montaTr(paciente);
 
-    var tabela = document.querySelector("#tabela-pacientes");
-    tabela.appendChild(pacienteTr);
-    form.reset();
+    adicionaPacienteNaTabela(paciente);
 
-    var mensagensErro = document.querySelector("#mensagens-erro");
+    form.reset();    
+    let mensagensErro = document.querySelector("#mensagens-erro");
     mensagensErro.innerHTML = "";
 });
+
+function adicionaPacienteNaTabela(paciente) {
+    let pacienteTr = montaTr(paciente);
+    let tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+}
 
 
 function obtemPacienteDoFormulario(form) {
 
-    var paciente = {
+    let paciente = {
         nome: form.nome.value,
         peso: form.peso.value,
         altura: form.altura.value,
@@ -35,7 +37,7 @@ function obtemPacienteDoFormulario(form) {
 }
 
 function montaTd(dado, classe) {
-    var td = document.createElement("td");
+    let td = document.createElement("td");
     td.classList.add(classe);
     td.textContent = dado;
 
@@ -44,7 +46,7 @@ function montaTd(dado, classe) {
 
 function montaTr(paciente) {
     //Cria TR
-    var pacienteTr = document.createElement("tr");
+    let pacienteTr = document.createElement("tr");
     pacienteTr.classList.add("paciente");
     //Cria as TD's e a adiciona dentro da TR
     pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
@@ -58,7 +60,7 @@ function montaTr(paciente) {
 
 function validaPaciente(paciente) {
 
-    var erros = [];
+    let erros = [];
 
     if (paciente.nome.length == 0) {
         erros.push("O nome não pode ser em branco");
@@ -88,11 +90,11 @@ function validaPaciente(paciente) {
 }
 
 function exibeMensagensDeErro(erros) {
-    var ul = document.querySelector("#mensagens-erro");
+    let ul = document.querySelector("#mensagens-erro");
     ul.innerHTML = "";
 
     erros.forEach(function(erro) {
-        var li = document.createElement("li");
+        let li = document.createElement("li");
         li.textContent = erro;
         ul.appendChild(li);
     });
