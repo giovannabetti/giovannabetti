37059a6adbfeb3316c50a158ab1a2a883340d465
Giovanna Betti <giovannabetti@gmail.com>
2021-02-25 19:11:35 -0300

    timer functions implemented

--- code/giovannabetti/next-level-week-04/moveit-next/src/components/ChallengeBox.tsx
+++ code/giovannabetti/next-level-week-04/moveit-next/src/components/ChallengeBox.tsx
@@ -1,18 +1,21 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
+
 import styles from "../styles/components/ChallengeBox.css";
 
 export function ChallengeBox() {
-  const hasActiveChallenge = true;
+  const { activeChallenge, resetChallenge } = useContext(ChallengesContext);
 
   return(
     <div className={styles.challengeBoxContainer}>
-      { hasActiveChallenge ? (
+      { activeChallenge ? (
         <div className={styles.challengeActive}>
-          <header>Ganhe 400 xp</header>
+          <header>Ganhe {activeChallenge.amount} xp</header>
 
           <main>
-            <img src="icons/body.svg" />
+            <img src={`icons/${activeChallenge.type}.svg`} />
             <strong>Novo Desafio</strong>
-            <p>Levante e faça uma caminhada de 3 minutos</p>
+            <p>{activeChallenge.description}</p>
           </main>
 
           <footer>
@@ -24,6 +27,7 @@ export function ChallengeBox() {
             <button
               type="button"
               className={styles.challengeSucceededButton}
+              onClick={resetChallenge}
             >
               Completei
             </button>
--- code/giovannabetti/next-level-week-04/moveit-next/src/components/Countdown.tsx
+++ code/giovannabetti/next-level-week-04/moveit-next/src/components/Countdown.tsx
@@ -1,10 +1,13 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 
 import styles from '../styles/components/Countdown.modules.css';
 
 let countdownTimeout: NodeJS.Timeout;
 
 export function Countdown() {
+  const { startNewChallenge } = useContext(ChallengesContext);
+
   const [time, setTime] = useState(25 * 60);
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
