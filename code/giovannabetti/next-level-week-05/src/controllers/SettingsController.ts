3b8711ef4224f3d7525a3e9d1e254d209cbe91f3
Giovanna Betti <giovannabetti@gmail.com>
2021-04-23 12:26:35 -0300

    finalizando a aula 04

--- code/giovannabetti/next-level-week-05/src/controllers/SettingsController.ts
+++ code/giovannabetti/next-level-week-05/src/controllers/SettingsController.ts
@@ -13,9 +13,30 @@ class SettingsController {
       return response.json(settings);
     } catch (error) {
       return response.status(400).json({
-        message: err.message,
+        message: error.message,
       });
     }
+  }
+
+  async findByUsername(request: Request, response: Response) {
+    const { username } = request.params;
+
+    const settingsService = new SettingsService();
+
+    const settings = await settingsService.findByUsername(username);
+
+    return response.json(settings);
+  }
+
+  async update(request: Request, response: Response) {
+    const { username } = request.params;
+    const { chat } = request.body;
+
+    const settingsService = new SettingsService();
+
+    const settings = await settingsService.update(username, chat);
+
+    return response.json(settings);
   }
 }
 
--- code/giovannabetti/next-level-week-05/src/server.ts
+++ code/giovannabetti/next-level-week-05/src/server.ts
@@ -1,29 +1,26 @@
-import express from "express";
-import "./database";
-import { routes } from "./routes";
+import { http } from "./http";
+import "./websocket/client";
 
-const app = express();
+http.listen(3333, () => console.log("Server is running"));
 
-app.use(express.json());
-app.use(routes);
-
+/*
 app.get("/", (request, response) => {
   return response.send("Olá, NLW 05");
-  /*
   Poderia ser gerado um json:
   return response.json({
     message: "Olá, NLW 05",
   });
-  */
 });
+
 
 app.post("/", (request, response) => {
   return response.json({ message: "User salvo com sucesso!" });
 });
-// Podemos usar Imsonia, Postman, Postwoman...
+  Podemos usar Imsonia, Postman, Postwoman...
+*/
 
-app.listen(3333, () => console.log("Server is running"));
 
 // missãoespacial
 // embuscadoproximonivel
 // astronautas
+// universoinfinito
