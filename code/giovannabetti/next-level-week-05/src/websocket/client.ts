e581a9b09c89952e94d83d0e0c56d2116cd7ed4c
Giovanna Betti <giovannabetti@gmail.com>
2021-04-23 16:35:45 -0300

    finalizando dia 05 no NLW

--- code/giovannabetti/next-level-week-05/src/websocket/client.ts
+++ code/giovannabetti/next-level-week-05/src/websocket/client.ts
@@ -46,8 +46,32 @@ io.on("connect", (socket) => {
 
     await messagesService.create({
       text,
-      user_id
-    })
-    // Salvar a conexao com o socket_id, user_id, infos da tabela de conexao
+      user_id,
+    });
+
+    const allMessages = await messagesService.listByUser(user_id);
+
+    socket.emit("client_list_all_messages", allMessages);
+
+    const allUsers = await connectionsService.findAllWithoutAdmin();
+    io.emit("admin_list_all_users", allUsers);
   });
+
+  socket.on("client_send_to_admin", async (params) => {
+    const { text, socket_admin_id } = params;
+
+    const socket_id = socket.id;
+
+    const { user_id } = await connectionsService.findBySocketID(socket_id)
+
+    const message = await messagesService.create({
+      text,
+      user_id,
+    });
+
+    io.to(socket_admin_id).emit("admin_receive_message", {
+      message,
+      socket_id
+    })
+  })
 });
