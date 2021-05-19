6fcf12311c3c000834918bdc9006e75e3518506c
Giovanna Betti <giovannabetti@gmail.com>
2021-05-19 19:26:31 -0300

    project finished - nodejs api with mvc and repository

--- code/giovannabetti/alura/nodejs/nodejs-streaming-e-repositorio/models/atendimentos.js
+++ code/giovannabetti/alura/nodejs/nodejs-streaming-e-repositorio/models/atendimentos.js
@@ -1,55 +1,64 @@
-const moment = require('moment');
-const axios = require('axios');
-const conexao = require('../infraestrutura/conexao');
-const repositorio = require('../repositorios/atendimento');
+const moment = require('moment')
+const axios = require('axios')
+const conexao = require('../infraestrutura/database/conexao')
+const repositorio = require('../repositorios/atendimento')
 
 class Atendimento {
-    adiciona(atendimento) {
-        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
-        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
-        
-        const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
-        const clienteEhValido = atendimento.cliente.length >= 5
+    constructor() {
+        this.dataEhValida = ({ data, dataCriacao }) =>
+            moment(data).isSameOrAfter(dataCriacao)
+        this.clienteEhValido = tamanho => tamanho >= 5
 
-        const validacoes = [
+        this.valida = parametros =>
+            this.validacoes.filter(campo => {
+                const { nome } = campo
+                const parametro = parametros[nome]
+
+                return !campo.valido(parametro)
+            })
+
+        this.validacoes = [
             {
                 nome: 'data',
-                valido: dataEhValida,
+                valido: this.dataEhValida,
                 mensagem: 'Data deve ser maior ou igual a data atual'
             },
             {
                 nome: 'cliente',
-                valido: clienteEhValido,
+                valido: this.clienteEhValido,
                 mensagem: 'Cliente deve ter pelo menos cinco caracteres'
             }
         ]
+    }
 
-        const erros = validacoes.filter(campo => !campo.valido)
+    adiciona(atendimento) {
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format(
+            'YYYY-MM-DD HH:MM:SS'
+        )
+
+        const parametros = {
+            data: { data, dataCriacao },
+            cliente: { tamanho: atendimento.cliente.length }
+        }
+
+        const erros = this.valida(parametros)
         const existemErros = erros.length
 
-        if(existemErros) {
-          return new Promise((resolve, reject) => reject(erros))
+        if (existemErros) {
+            return new Promise((resolve, reject) => reject(erros))
         } else {
-            const atendimentoDatado = {...atendimento, dataCriacao, data}
-    
-            return repositorio.adiciona(atendimentoDatado)
-            .then(resultados => {
-              const id = resultados.insertId
-              return {...atendimento, id}
+            const atendimentoDatado = { ...atendimento, dataCriacao, data }
+
+            return repositorio.adiciona(atendimentoDatado).then(resultados => {
+                const id = resultados.insertId
+                return { ...atendimento, id }
             })
         }
     }
 
-    lista(res) {
-        const sql = 'SELECT * FROM Atendimentos'
-
-        conexao.query(sql, (erro, resultados) => {
-            if(erro) {
-                res.status(400).json(erro)
-            } else {
-                res.status(200).json(resultados)
-            }
-        })
+    lista() {
+        return repositorio.lista()
     }
 
     buscaPorId(id, res) {
@@ -58,27 +67,31 @@ class Atendimento {
         conexao.query(sql, async (erro, resultados) => {
             const atendimento = resultados[0]
             const cpf = atendimento.cliente
-            if(erro) {
+            if (erro) {
                 res.status(400).json(erro)
             } else {
-              const { data } = await axios.get(`http://localhost:8082/${cpf}`)
-              atendimento.cliente = data
+                const { data } = await axios.get(`http://localhost:8082/${cpf}`)
+
+                atendimento.cliente = data
+
                 res.status(200).json(atendimento)
             }
         })
     }
 
     altera(id, valores, res) {
-        if(valores.data) {
-            valores.data = moment(valores.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
-        }      
+        if (valores.data) {
+            valores.data = moment(valores.data, 'DD/MM/YYYY').format(
+                'YYYY-MM-DD HH:MM:SS'
+            )
+        }
         const sql = 'UPDATE Atendimentos SET ? WHERE id=?'
 
         conexao.query(sql, [valores, id], (erro, resultados) => {
-            if(erro) {
+            if (erro) {
                 res.status(400).json(erro)
             } else {
-                res.status(200).json({...valores, id})
+                res.status(200).json({ ...valores, id })
             }
         })
     }
@@ -87,13 +100,13 @@ class Atendimento {
         const sql = 'DELETE FROM Atendimentos WHERE id=?'
 
         conexao.query(sql, id, (erro, resultados) => {
-            if(erro) {
+            if (erro) {
                 res.status(400).json(erro)
             } else {
-                res.status(200).json({id})
+                res.status(200).json({ id })
             }
         })
     }
-};
+}
 
-module.exports = new Atendimento;
+module.exports = new Atendimento()
--- code/giovannabetti/alura/nodejs/nodejs-streaming-e-repositorio/repositorios/atendimento.js
+++ code/giovannabetti/alura/nodejs/nodejs-streaming-e-repositorio/repositorios/atendimento.js
@@ -1,10 +1,16 @@
-const query = require('../infraestrutura/database/queries');
+const query = require('../infraestrutura/database/queries')
 
 class Atendimento {
-  adiciona(atendimento) {
-    const sql = 'INSERT INTO Atendimentos SET ?'
-    return query(sql, atendimento)
-  }
+    adiciona(atendimento) {
+        const sql = 'INSERT INTO Atendimentos SET ?'
+        return query(sql, atendimento)
+    }
+
+    lista() {
+        const sql = 'SELECT * FROM Atendimentos'
+
+        return query(sql)
+    }
 }
 
-module.exports = new Atendimento();
+module.exports = new Atendimento()
