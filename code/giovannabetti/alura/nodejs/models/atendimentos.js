acfc7f13d0001ba7fba74afcf3b47ca455cf7578
Giovanna Betti <giovannabetti@gmail.com>
2021-05-18 23:52:18 -0300

    node js rest project by alura

---
+++ code/giovannabetti/alura/nodejs/models/atendimentos.js
@@ -0,0 +1,22 @@
+const moment = require('moment');
+const conexao = require('../infraestrutura/conexao');
+
+class Atendimento {
+  adiciona(atendimento, res) {
+    const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
+    const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+    const atendimentoDatado = {...atendimento, dataCriacao, data}
+    
+    const sql = 'INSERT INTO Atendimentos SET ?'
+
+    conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+      if(erro) {
+        res.status(400).json(erro)
+      } else {
+        res.status(201).json(resultados)
+      }
+    })
+  }
+}
+
+module.exports = new Atendimento
