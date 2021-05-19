0f78b0a29ac2c7ff71c42d3bff8a8134527e687a
Giovanna Betti <giovannabetti@gmail.com>
2021-05-19 00:33:25 -0300

    adjusting http requests with get method - using imsonia and myqsl workbench

--- code/giovannabetti/alura/nodejs/controllers/atendimentos.js
+++ code/giovannabetti/alura/nodejs/controllers/atendimentos.js
@@ -1,7 +1,15 @@
 const Atendimento = require('../models/atendimentos')
 
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando um GET'))
+    app.get('/atendimentos', (req, res) => {
+      Atendimento.lista(res)
+    })
+
+    app.get('/atendimentos/:id', (req, res) => {
+      const id = parseInt(req.params.id)
+
+      Atendimento.buscaPorId(id, res)
+    })
 
     app.post('/atendimentos', (req, res) => {
         const atendimento = req.body
--- code/giovannabetti/alura/nodejs/models/atendimentos.js
+++ code/giovannabetti/alura/nodejs/models/atendimentos.js
@@ -3,20 +3,70 @@ const conexao = require('../infraestrutura/conexao');
 
 class Atendimento {
   adiciona(atendimento, res) {
-    const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
-    const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
-    const atendimentoDatado = {...atendimento, dataCriacao, data}
+    const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
+    const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss');
     
-    const sql = 'INSERT INTO Atendimentos SET ?'
+    const dataEhValida = moment(data).isSameOrAfter(dataCriacao);
+    const clienteEhValido = atendimento.cliente.length >= 5;
 
-    conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+    const validacoes = [
+      {
+        nome: 'data',
+        valido: dataEhValida,
+        mensagem: 'Data deve ser maior ou igual à data atual',
+      },
+      {
+        nome: 'cliente',
+        valido: clienteEhValido,
+        mensagem: 'Cliente deve ter pelo menos cinco caracteres'
+      }
+    ]
+
+    const erros = validacoes.filter(campo => !campo.valido)
+    const existemErros = erros.length
+    
+    if(existemErros) {
+      res.status(400).json(erros)
+    } else {
+      const atendimentoDatado = {...atendimento, dataCriacao, data};
+    
+      const sql = 'INSERT INTO Atendimentos SET ?';
+  
+      conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+        if(erro) {
+          res.status(400).json(erro)
+        } else {
+          res.status(201).json(resultados)
+        }
+      })
+    }
+  };
+
+  lista(res) {
+    const sql = 'SELECT * FROM Atendimentos';
+
+    conexao.query(sql, (erro, resultados) => {
+      if(err0) {
+        res.status(400).json(erro)
+      } else {
+        res.status(200).json(resultados)
+      }
+    })
+  };
+
+  buscaPorId(id, res) {
+    const sql = `SELECT * FROM Atendimentos WHERE id=${id}`;
+
+    conexao.query(sql, (erro, resultados) => {
+      const atendimento = resultado[0];
+
       if(erro) {
         res.status(400).json(erro)
       } else {
-        res.status(201).json(resultados)
+        res.status(200).json(atendimento)
       }
     })
   }
-}
+};
 
-module.exports = new Atendimento
+module.exports = new Atendimento;
