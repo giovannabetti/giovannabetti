e3c9e8fca2ffffa32b437bff2ffd5ff107503e6a
Giovanna Betti <giovannabetti@gmail.com>
2021-05-28 22:06:14 -0300

    Config CRUD initial routes

--- code/giovannabetti/alura/node-str/src/app.js
+++ code/giovannabetti/alura/node-str/src/app.js
@@ -1,17 +1,41 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const router = express.Router();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 const route = router.get('/', (req, res, next) => {
   res.status(200).send({
     title: "Node Store API",
-    version: "0.0.1"
+    version: "0.0.2"
   });
 });
 
+const create = router.post('/', (req, res, next) => {
+  res.status(201).send(req.body);
+});
+
+const put = router.put('/:id', (req, res, next) => {
+  // recovering params from url
+  const id = req.params.id;
+  res.status(201).send({
+    id: id,
+    item: req.body
+  });
+});
+
+const del = router.delete('/', (req, res, next) => {
+  res.status(200).send(req.body);
+});
+
 app.use('/', route);
+app.use('/products', create);
+app.use('/products', put);
+app.use('/products', del);
 
 module.exports = app;
