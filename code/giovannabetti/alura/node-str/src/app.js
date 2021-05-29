601d10b2380767df500dbe69dbc1e78bcf1d67fc
Giovanna Betti <giovannabetti@gmail.com>
2021-05-28 22:20:36 -0300

    Improving app file and routes

--- code/giovannabetti/alura/node-str/src/app.js
+++ code/giovannabetti/alura/node-str/src/app.js
@@ -1,41 +1,19 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 const router = express.Router();
 
+// Loading routes
+const indexRoute = require("./routes/index-route");
+const productRoute = require("./routes/product-route");
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const route = router.get('/', (req, res, next) => {
-  res.status(200).send({
-    title: "Node Store API",
-    version: "0.0.2"
-  });
-});
-
-const create = router.post('/', (req, res, next) => {
-  res.status(201).send(req.body);
-});
-
-const put = router.put('/:id', (req, res, next) => {
-  // recovering params from url
-  const id = req.params.id;
-  res.status(201).send({
-    id: id,
-    item: req.body
-  });
-});
-
-const del = router.delete('/', (req, res, next) => {
-  res.status(200).send(req.body);
-});
-
-app.use('/', route);
-app.use('/products', create);
-app.use('/products', put);
-app.use('/products', del);
+app.use("/", indexRoute);
+app.use("/products", productRoute);
 
 module.exports = app;
