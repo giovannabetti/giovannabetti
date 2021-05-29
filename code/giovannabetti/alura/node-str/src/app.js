f5c4e8f6a1d6ba02748d3838e0477a4d62a1c83b
Giovanna Betti <giovannabetti@gmail.com>
2021-05-28 23:59:03 -0300

    Configuring database nosql with MongoDB Atlas and using Studio 3T

--- code/giovannabetti/alura/node-str/src/app.js
+++ code/giovannabetti/alura/node-str/src/app.js
@@ -2,9 +2,16 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 const router = express.Router();
+
+// Connect to database
+mongoose.connect('mongodb+srv://balta:balta@nodestore.8em4e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+
+// Loading models
+const Product = require('./models/product');
 
 // Loading routes
 const indexRoute = require("./routes/index-route");
--- code/giovannabetti/alura/node-str/src/controllers/product-controller.js
+++ code/giovannabetti/alura/node-str/src/controllers/product-controller.js
@@ -1,16 +1,42 @@
-'use strict';
+"use strict";
+
+const mongoose = require("mongoose");
+const Product = mongoose.model("Product");
+
+exports.get = (req, res, net) => {
+  Product
+    .find({})
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((e) => {
+      res
+        .status(400)
+        .send(e);
+    });
+}
 
 exports.post = (req, res, next) => {
-  res.status(201).send(req.body);
+  let product = new Product(req.body);
+  product
+    .save()
+    .then((x) => {
+      res.status(201).send({ message: "Produto cadastrado com sucesso!" });
+    })
+    .catch((e) => {
+      res
+        .status(400)
+        .send({ message: "Falha ao cadastrar o produto", data: e });
+    });
 };
 
 exports.put = (req, res, next) => {
-    // recovering params from url
-    const id = req.params.id;
-    res.status(201).send({
-      id: id,
-      item: req.body
-    });
+  // recovering params from url
+  const id = req.params.id;
+  res.status(201).send({
+    id: id,
+    item: req.body,
+  });
 };
 
 exports.delete = (req, res, next) => {
