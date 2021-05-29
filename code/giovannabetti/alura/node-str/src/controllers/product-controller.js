1b903d458ab7950736928e6edae5d47b631019e5
Giovanna Betti <giovannabetti@gmail.com>
2021-05-29 02:32:53 -0300

    Refactoring controllers

--- code/giovannabetti/alura/node-str/src/controllers/product-controller.js
+++ code/giovannabetti/alura/node-str/src/controllers/product-controller.js
@@ -2,24 +2,79 @@
 
 const mongoose = require("mongoose");
 const Product = mongoose.model("Product");
+const ValidationContract = require("../validators/fluent-validator");
+const repository = require("../repositories/product-repository");
 
-exports.get = (req, res, net) => {
-  Product
-    .find({})
+exports.get = (req, res, next) => {
+  repository
+    .get()
     .then((data) => {
       res.status(200).send(data);
     })
     .catch((e) => {
-      res
-        .status(400)
-        .send(e);
+      res.status(400).send(e);
     });
-}
+};
+
+exports.getBySlug = (req, res, next) => {
+  repository
+    .getBySlug(req.params.slug)
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+};
+
+exports.getById = (req, res, next) => {
+  repository
+    .getById(req.params.id)
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+};
+
+exports.getByTag = (req, res, next) => {
+  repository
+    .getByTag(req.params.tag)
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+};
 
 exports.post = (req, res, next) => {
-  let product = new Product(req.body);
-  product
-    .save()
+  let contract = new ValidationContract();
+  contract.hasMinLen(
+    req.body.title,
+    3,
+    "O título deve conter pelo menos três caracteres"
+  );
+  contract.hasMinLen(
+    req.body.slug,
+    3,
+    "O slug deve conter pelo menos três caracteres"
+  );
+  contract.hasMinLen(
+    req.body.description,
+    3,
+    "A descrição deve conter pelo menos três caracteres"
+  );
+
+  // If contracts are invalid
+  if (!contract.isValid()) {
+    res.status(400).send(contract.errors()).end();
+    return;
+  }
+
+  repository
+    .create(req.body)
     .then((x) => {
       res.status(201).send({ message: "Produto cadastrado com sucesso!" });
     })
@@ -31,14 +86,33 @@ exports.post = (req, res, next) => {
 };
 
 exports.put = (req, res, next) => {
-  // recovering params from url
-  const id = req.params.id;
-  res.status(201).send({
-    id: id,
-    item: req.body,
-  });
+  repository
+    .update(req.params.id, req.body)
+    .then((x) => {
+      res.status(200).send({
+        message: "Produto atualizado com sucesso!",
+      });
+    })
+    .catch((e) => {
+      res.status(400).send({
+        message: "Falha ao atualizar produto",
+        data: e,
+      });
+    });
 };
 
 exports.delete = (req, res, next) => {
-  res.status(200).send(req.body);
+  repository
+    .delete(req.body.id)
+    .then((x) => {
+      res.status(200).send({
+        message: "Produto removido com sucesso!",
+      });
+    })
+    .catch((e) => {
+      res.status(400).send({
+        message: "Falha ao remover produto",
+        data: e,
+      });
+    });
 };
--- code/giovannabetti/alura/node-str/src/routes/product-route.js
+++ code/giovannabetti/alura/node-str/src/routes/product-route.js
@@ -5,6 +5,9 @@ const router = express.Router();
 const controller = require('../controllers/product-controller');
 
 router.get('/', controller.get);
+router.get('/:slug', controller.getBySlug);
+router.get('/admin/:id', controller.getById);
+router.get('/tags/:tag', controller.getByTag);
 router.post('/', controller.post);
 router.put('/:id', controller.put);
 router.delete('/', controller.delete);
