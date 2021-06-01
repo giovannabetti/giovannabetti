debfa46e7547f37391882c8f3f662aef912408cb
Giovanna Betti <giovannabetti@gmail.com>
2021-06-01 00:04:32 -0300

    Crypto password with md5

--- code/giovannabetti/alura/node-str/src/controllers/customer-controller.js
+++ code/giovannabetti/alura/node-str/src/controllers/customer-controller.js
@@ -2,6 +2,7 @@
 
 const ValidationContract = require("../validators/fluent-validator");
 const repository = require("../repositories/customer-repository");
+const md5 = require("md5");
 
 exports.post = async (req, res, next) => {
   let contract = new ValidationContract();
@@ -27,7 +28,12 @@ exports.post = async (req, res, next) => {
   }
 
   try {
-    await repository.create(req.body);
+    await repository.create({
+      name: req.body.name,
+      email: req.body.email,
+      // Just md5 is enough and is difficulty to broke crypto, so add a server's key for more protection of data.
+      password: md5(req.body.password + global.SALT_KEY),
+    });
     res.status(201).send({
       message: "Cliente cadastrado com sucesso!",
     });
