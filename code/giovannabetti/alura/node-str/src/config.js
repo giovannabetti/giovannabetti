760bd3c7830339cbfd1ab3c81a2557d79430c741
Giovanna Betti <giovannabetti@gmail.com>
2021-06-01 01:16:52 -0300

    Installing sendgrid

--- code/giovannabetti/alura/node-str/src/config.js
+++ code/giovannabetti/alura/node-str/src/config.js
@@ -2,7 +2,7 @@ global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
 
 module.exports = {
-    connectionString: 'mongodb+srv://balta:balta@nodestore.8em4e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    sendgridKey: 'SUA CHAVE',
-    containerConnectionString: 'SUA CONNECTION STRING'
+  connectionString: 'mongodb+srv://balta:balta@nodestore.8em4e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  sendgridKey: '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',
+  containerConnectionString: 'SUA CONNECTION STRING'
 };
---
+++ code/giovannabetti/alura/node-str/src/services/email-server.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let config = require('../config');
+let sendgrid = require('sendgrid')(config.sendgridKey);
+
+exports.send = async (to, subject, body) => {
+  sendgrid.send({
+    to: to,
+    from: 'hello@balta.io',
+    html: body
+  });
+};
