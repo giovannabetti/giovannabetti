3b1b424f8cfa9cc1b1914546e5330f0a6a48a5ce
Giovanna Betti <giovannabetti@gmail.com>
2021-05-31 23:56:36 -0300

    Changing hard database access and replacing by config

--- code/giovannabetti/alura/node-str/src/app.js
+++ code/giovannabetti/alura/node-str/src/app.js
@@ -3,12 +3,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const config = require("config");
 
 const app = express();
 const router = express.Router();
 
 // Connect to database
-mongoose.connect('mongodb+srv://balta:balta@nodestore.8em4e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+mongoose.connect(config.connectionString);
 
 // Loading models
 const Product = require('./models/product');
--- code/giovannabetti/alura/node-str/src/repositories/order-repository.js
+++ code/giovannabetti/alura/node-str/src/repositories/order-repository.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 const Order = mongoose.model("Order");
 
 exports.get = async (data) => {
-  let res = await Order.find({});
+  let res = await Order
+    .find({}, 'number status customer items')
+    .populate('customer', 'name')
+    .populate('items.product', 'title');
   return res;
 };
 
