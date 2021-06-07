7f366f96035eb5281d9117ba30961106683b61ce
Giovanna Betti <giovannabetti@gmail.com>
2021-06-07 18:34:41 -0300

    Controller implemented nd responsabilities defined

--- code/giovannabetti/alura/orm-sequelize-nodejs/api/index.js
+++ code/giovannabetti/alura/orm-sequelize-nodejs/api/index.js
@@ -1,15 +1,10 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const routes = require('./routes');
 
 const app = express();
-
-app.use(bodyParser.json());
-
 const port = 3000;
 
-app.get('/teste', (req, res) => res
-  .status(200)
-  .send({ message: 'boas-vindas à API'}))
+routes(app);
 
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
 
