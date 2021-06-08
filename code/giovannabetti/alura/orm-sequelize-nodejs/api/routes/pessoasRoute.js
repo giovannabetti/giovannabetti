692f5b2485155ff581fc45ef988bdcc160ed8690
Giovanna Betti <giovannabetti@gmail.com>
2021-06-07 21:31:18 -0300

    API finished

--- code/giovannabetti/alura/orm-sequelize-nodejs/api/routes/pessoasRoute.js
+++ code/giovannabetti/alura/orm-sequelize-nodejs/api/routes/pessoasRoute.js
@@ -8,5 +8,9 @@ router.get('/pessoas/:id', PessoaController.pegaUmaPessoa);
 router.post('/pessoas', PessoaController.criaPessoa);
 router.put('/pessoas/:id', PessoaController.atualizaPessoa);
 router.delete('/pessoas/:id', PessoaController.apagaPessoa);
+router.get('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.pegaUmaMatricula);
+router.post('/pessoas/:estudanteId/matricula', PessoaController.criaMatricula);
+router.put('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.atualizaMatricula);
+router.delete('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.apagaMatricula);
 
 module.exports = router;
