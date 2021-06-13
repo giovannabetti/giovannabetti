2c20fa9417b2efd61c63bccba6675c1d8d371f82
Giovanna Betti <giovannabetti@gmail.com>
2021-06-13 06:58:30 -0300

    Sending real emails

--- code/giovannabetti/alura/blog-do-codigo-2-aula0/src/usuarios/emails.js
+++ code/giovannabetti/alura/blog-do-codigo-2-aula0/src/usuarios/emails.js
@@ -1,14 +1,36 @@
 const nodemailer = require('nodemailer');
 
+const configuracaoEmailProducao = {
+  host: process.env.EMAIL_HOST,
+  auth: {
+    user: process.env.EMAIL_USUARIO,
+    pass: process.env.EMAIL_SENHA
+  },
+  secure: true
+}
+
+const configuracaoEmailTeste = (contaTeste) => ({
+  host: 'smtp.ethereal.email',
+  auth: contraTeste,
+})
+
+async function criaConfiguracaoEmail() {
+  if (process.env.NODE_ENV === 'production') {
+    return configuracaoEmailProducao;
+  } else {
+    const contraTeste = await nodemailer.createTestAccount();
+    return configuracaoEmailTeste(contaTeste)
+  }
+}
 class Email {
   async enviaEmail() {
-    const contraTeste = await nodemailer.createTestAccount();
-    const transportador = nodemailer.createTransport({
-      host: 'smtp.ethereal.email',
-      auth: contraTeste,
-    });
+    const configuracaoEmail = await criaConfiguracaoEmail();
+    const transportador = nodemailer.createTransport(configuracaoEmail);
     const info = await transportador.sendMail(this);
-    console.log( 'URL' + nodemailer.getTestMessageUrl(info));
+
+    if (process.env.NODE_ENV !== 'production') {
+      console.log( 'URL' + nodemailer.getTestMessageUrl(info));
+    }
   }
 }
 
