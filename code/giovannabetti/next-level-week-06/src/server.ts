3847d12a4e6f3097f4b9d39f90768f87d8f27649
Giovanna Betti <giovannabetti@gmail.com>
2021-06-30 00:55:56 -0300

    Errors, middlewares and routes adjusts

--- code/giovannabetti/next-level-week-06/src/server.ts
+++ code/giovannabetti/next-level-week-06/src/server.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import "express-async-errors";
 import { router } from "./routes";
 import "./database";
 
@@ -7,5 +8,19 @@ const app = express();
 
 app.use(express.json());
 app.use(router);
+
+// middleware
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if(err instanceof Error) {
+    return response.status(400).json({
+      error: err.message
+    })
+  }
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal Server Error"
+  })
+})
 
 app.listen(3000, () => console.log('Server is running'));
