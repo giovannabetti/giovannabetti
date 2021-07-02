b81b24c5d1fcf4bef7fa9354f97577a57e0f6f64
Giovanna Betti <giovannabetti@gmail.com>
2021-07-02 05:53:57 -0300

    adicionando cors

--- code/giovannabetti/next-level-week-06/src/server.ts
+++ code/giovannabetti/next-level-week-06/src/server.ts
@@ -3,9 +3,11 @@ import express, { Request, Response, NextFunction } from 'express';
 import "express-async-errors";
 import { router } from "./routes";
 import "./database";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
