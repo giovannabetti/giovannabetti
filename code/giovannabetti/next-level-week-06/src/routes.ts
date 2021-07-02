68b82a3993dd56b43f8ee3b31ae84dbd31085fa3
Giovanna Betti <giovannabetti@gmail.com>
2021-07-02 05:49:42 -0300

    lista users sem mostrar senha e finalizando app

--- code/giovannabetti/next-level-week-06/src/routes.ts
+++ code/giovannabetti/next-level-week-06/src/routes.ts
@@ -8,6 +8,7 @@ import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
 import { ListUserSendComplimentsController } from "./controllers/ListUserSendComplimentsController";
 import { ListUserReceiveComplimentsController } from "./controllers/ListUserReceiveComplimentsController";
 import { ListTagsController } from "./controllers/ListTagsController";
+import { ListUserController } from "./controllers/ListUserController";
 
 const router = Router();
 
@@ -18,14 +19,16 @@ const createComplimentController = new CreateComplimentController();
 const listUserSendComplimentsController = new ListUserSendComplimentsController();
 const listUserReceiveComplimentsController = new ListUserReceiveComplimentsController();
 const listTagsController = new ListTagsController();
+const listUsersController = new ListUserController();
 
 router.post("/tags", ensureAuthenticated, ensureAdmin, createTagController.handle);
 router.post("/users", createUserController.handle);
 router.post("/login", authenticateUserController.handle);
 router.post("/compliments", ensureAuthenticated, createComplimentController.handle);
 
-router.get("users/compliments/send", ensureAuthenticated, listUserSendComplimentsController.handle)
-router.get("users/compliments/receive", ensureAuthenticated, listUserReceiveComplimentsController.handle)
-router.get("tags", ensureAuthenticated, listTagsController.handle)
+router.get("/users/compliments/send", ensureAuthenticated, listUserSendComplimentsController.handle)
+router.get("/users/compliments/receive", ensureAuthenticated, listUserReceiveComplimentsController.handle)
+router.get("/tags", ensureAuthenticated, listTagsController.handle)
+router.get("/users", ensureAuthenticated, listUsersController.handle)
 
 export { router }
