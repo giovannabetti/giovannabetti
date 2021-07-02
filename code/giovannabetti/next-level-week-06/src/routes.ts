0ebe78f3d3eae4c309564ee18cdd0f1331404e94
Giovanna Betti <giovannabetti@gmail.com>
2021-07-02 05:18:59 -0300

    Listando elogios enviados e recebidos

--- code/giovannabetti/next-level-week-06/src/routes.ts
+++ code/giovannabetti/next-level-week-06/src/routes.ts
@@ -5,6 +5,8 @@ import { ensureAdmin } from "./middlewares/ensureAdmin";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateComplimentController } from "./controllers/CreateComplimentController";
 import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
+import { ListUserSendComplimentsController } from "./controllers/ListUserSendComplimentsController";
+import { ListUserReceiveComplimentsController } from "./controllers/ListUserReceiveComplimentsController";
 
 const router = Router();
 
@@ -12,10 +14,15 @@ const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 const authenticateUserController = new AuthenticateUserController();
 const createComplimentController = new CreateComplimentController();
+const listUserSendComplimentsController = new ListUserSendComplimentsController();
+const listUserReceiveComplimentsController = new ListUserReceiveComplimentsController();
 
 router.post("/tags", ensureAuthenticated, ensureAdmin, createTagController.handle);
 router.post("/users", createUserController.handle);
 router.post("/login", authenticateUserController.handle);
 router.post("/compliments", ensureAuthenticated, createComplimentController.handle);
+
+router.get("users/compliments/send", ensureAuthenticated, listUserSendComplimentsController.handle)
+router.get("users/compliments/receive", ensureAuthenticated, listUserReceiveComplimentsController.handle)
 
 export { router }
